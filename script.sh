00df73abf5e6d0cffd060bb67e346fedd3439f12
huyikun.hyk <huyikun.hyk@alibaba-inc.com>
2024-01-16 10:34:51 +0800

    feat: update output.

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 npm i @serverless-devs/s -g  --registry=https://registry.npmmirror.com
 s config add --AccountID $1 --AccessKeyID $2 --AccessKeySecret $3 -a $4 -f
-s deploy --use-local -y
+echo 'Serverless-devs has been installed successfully.'
+s config get $4
