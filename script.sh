1293d5537a67269338b393a68268cbde3facb2b4
huyikun.hyk <huyikun.hyk@alibaba-inc.com>
2024-01-16 10:38:41 +0800

    feat: change output

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 npm i @serverless-devs/s -g  --registry=https://registry.npmmirror.com
 s config add --AccountID $1 --AccessKeyID $2 --AccessKeySecret $3 -a $4 -f
 echo 'Serverless-devs has been installed successfully.'
-s config get $4
+echo 'The access has been configured automatically, as shown bellow.'
+s config get -a $4
