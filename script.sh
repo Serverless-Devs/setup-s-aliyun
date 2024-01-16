2516e4eafc3e765aaf8d3bf39ca097888c0522a6
huyikun.hyk <huyikun.hyk@alibaba-inc.com>
2024-01-16 10:51:00 +0800

    update: docs change.

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
+echo 'Serverless-devs installing.'
 npm i @serverless-devs/s -g  --registry=https://registry.npmmirror.com
 s config add --AccountID $1 --AccessKeyID $2 --AccessKeySecret $3 -a $4 -f
+echo '################################################'
 echo 'Serverless-devs has been installed successfully.'
 echo 'The access has been configured automatically, as shown bellow.'
 s config get -a $4
