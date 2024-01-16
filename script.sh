4417aee4b45249022046583ffb92fb5c6eeda247
huyikun.hyk <huyikun.hyk@alibaba-inc.com>
2024-01-16 10:31:22 +0800

    feat: setup

--- script.sh
+++ script.sh
@@ -1,2 +1,3 @@
-echo 'helloworld'
-echo $1 $2 $3 $4
+npm i @serverless-devs/s -g  --registry=https://registry.npmmirror.com
+s config add --AccountID $1 --AccessKeyID $2 --AccessKeySecret $3 -a $4 -f
+s deploy --use-local -y
