11d8441e27e332520061acd96c1043011dcd967b
huyikun.hyk <huyikun.hyk@alibaba-inc.com>
2024-02-26 11:23:46 +0800

    update

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ version=$5
 # if [[ "${valid_version[@]}" =~ "${version}" ]]; then
 if [ ! -f "/usr/local/bin/s" ]; then
   echo 'Serverless-devs installing.'
-  npm install -g serverless-devs@${version}
+  npm install -g serverless-devs/s@${version}
   echo 'Serverless-devs has been installed successfully.'
   # curl "https://images.devsapp.cn/bin/s/${version}" --output /usr/local/bin/s
   # chmod 777 /usr/local/bin/s
